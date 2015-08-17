bfa388c68c46b89e6827a27a4c354692fea277a3
yijun <365657359@qq.com>
2015-08-17 21:03:16 +0800

    start

---
+++ DBFDecoder/src/EchoClientHandler.java
@@ -0,0 +1,39 @@
+import com.sun.org.apache.bcel.internal.ExceptionConstants;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by WYJ on 2015-08-17.
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+    static final String ECHO_REQ = "Hi,Lilinfeng.Welcome to Netty.$_";
+
+    public EchoClientHandler() {
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; 1 < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + "times receive server : [" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
